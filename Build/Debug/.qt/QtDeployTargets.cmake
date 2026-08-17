7a8f0c21e12f60e2577028cb8accdf37ba41ea56
kepler <kepler-444b@outlook.com>
2026-08-17 17:50:54 +0800

    删除编译文件

--- Build/Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ConfigTool_FILE D:/code/QT_ConfigTool/Build/Debug/ConfigTool.exe)
-set(__QT_DEPLOY_TARGET_ConfigTool_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ConfigTool_RUNTIME_DLLS D:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6SerialPort.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
--- Build/Debug/CMakeFiles/ConfigTool_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ConfigTool_autogen.
-# This may be replaced when dependencies are built.
